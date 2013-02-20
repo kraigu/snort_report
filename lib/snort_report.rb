f86c6552f30f4d5f1735138b9e15de19650988e9
cjshi <schengjie1007@hotmail.com>
2013-02-20 10:56:47 -0500

    Added one method of getting the path of a config file

--- lib/snort_report.rb
+++ lib/snort_report.rb
@@ -28,5 +28,12 @@ class Snort_report
 		)
 		return dbc
 	end
+	def self.path
+	    myc = Snort_report.parseconfig
+	    location = myc.get_value('file')['path']
+		return location
+	end	
 end
+
+
 
